0c3621e53d1a695bab047a1a62ea8dc1bd8839b1
amir0ghahremanian <amir0ghahremanian@outlook.com>
2024-04-29 23:56:13 +0330

    oops!

--- init.lua
+++ init.lua
@@ -9,7 +9,7 @@ if vim.g.neovide then
     vim.o.guifont = "JetBrains_Mono:h11"
     vim.g.neovide_transparency = 0.85
 
-    vim.keymap.set('n', '<C-f>',
+    vim.keymap.set('n', '<F11>',
 	function()
 	    vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen
 	end,
