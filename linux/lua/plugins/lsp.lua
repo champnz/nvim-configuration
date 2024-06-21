909d6f1e469a7562f02f7cd0e15c8b739a567325
amir0ghahremanian <amir0ghahremanian@outlook.com>
2024-06-21 12:25:57 +0330

    improvements

--- linux/lua/plugins/lsp.lua
+++ linux/lua/plugins/lsp.lua
@@ -49,10 +49,9 @@ NvimLspConfig.config = function()
 	vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 	vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 	vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-	vim.keymap.set("n", "<leader>rn", vim.lsp.buf.code_action, {})
-	vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
-	vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-	vim.keymap.set("n", "<C-i>", vim.lsp.buf.format)
+	vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+	vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 end
 
 return { CmpNvimLsp, LspKind, LuaSnip, NvimCmp, NvimLspConfig }
--- linux/lua/plugins/rust.lua
+++ linux/lua/plugins/rust.lua
@@ -4,4 +4,8 @@ Plugin.version = "^4" -- Recommended
 
 Plugin.lazy = false -- This plugin is already lazy
 
+Plugin.config = function()
+	vim.lsp.inlay_hint.enable(true)
+end
+
 return Plugin
