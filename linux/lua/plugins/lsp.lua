782d9083a9c375b2e1a2670e1e15fdf92053e06f
amir0ghahremanian <amir0ghahremanian@outlook.com>
2024-07-05 01:02:58 +0330

    linux: trouble

--- linux/lua/plugins/lsp.lua
+++ linux/lua/plugins/lsp.lua
@@ -85,13 +85,6 @@ NvimLspConfig.config = function()
 	local lspconfig = require("lspconfig")
 	lspconfig.clangd.setup({})
 	lspconfig.lua_ls.setup({})
-
-	vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-	vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-	vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-	vim.keymap.set("n", "<leader>F", vim.lsp.buf.format)
 end
 
 return { CmpNvimLsp, LspKind, LuaSnip, NvimCmp, NvimLspConfig }
---
+++ linux/lua/plugins/trouble.lua
@@ -0,0 +1,40 @@
+local Plugin = { "folke/trouble.nvim" }
+
+Plugin.opts = {}
+
+Plugin.cmd = "Trouble"
+
+Plugin.keys = {
+	{
+		"<leader>tx",
+		"<cmd>Trouble diagnostics toggle<cr>",
+		desc = "Diagnostics (Trouble)",
+	},
+	{
+		"<leader>tX",
+		"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+		desc = "Buffer Diagnostics (Trouble)",
+	},
+	{
+		"<leader>ts",
+		"<cmd>Trouble symbols toggle focus=false<cr>",
+		desc = "Symbols (Trouble)",
+	},
+	{
+		"<leader>tl",
+		"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+		desc = "LSP Definitions / references / ... (Trouble)",
+	},
+	{
+		"<leader>tL",
+		"<cmd>Trouble loclist toggle<cr>",
+		desc = "Location List (Trouble)",
+	},
+	{
+		"<leader>tQ",
+		"<cmd>Trouble qflist toggle<cr>",
+		desc = "Quickfix List (Trouble)",
+	},
+}
+
+return Plugin
