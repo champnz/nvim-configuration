05f162f6194f4eb4ad971a93fb45ee814eaa48a0
amir0ghahremanian <amir0ghahremanian@gmail.com>
2024-07-22 20:23:45 +0330

    lspconfig sorting

--- linux/lua/plugins/lsp.lua
+++ linux/lua/plugins/lsp.lua
@@ -54,18 +54,17 @@ NvimCmp.config = function()
 				end,
 			}),
 		},
-		-- sorting = {
-		-- 	comparators = {
-		-- 		cmp.config.compare.offset,
-		-- 		cmp.config.compare.exact,
-		-- 		cmp.config.compare.recently_used,
-		-- 		require("clangd_extensions.cmp_scores"),
-		-- 		cmp.config.compare.kind,
-		-- 		cmp.config.compare.sort_text,
-		-- 		cmp.config.compare.length,
-		-- 		cmp.config.compare.order,
-		-- 	},
-		-- },
+		sorting = {
+			comparators = {
+				cmp.config.compare.offset,
+				cmp.config.compare.exact,
+				cmp.config.compare.recently_used,
+				cmp.config.compare.kind,
+				cmp.config.compare.sort_text,
+				cmp.config.compare.length,
+				cmp.config.compare.order,
+			},
+		},
 	})
 
 	-- Set up lspconfig.
