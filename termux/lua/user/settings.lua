3b6face890ffdf0513e3ec4a0e29f50ab2260bed
amir0ghahremanian <amir0ghahremanian@gmail.com>
2025-03-31 21:42:22 +0330

    termux: oops

--- termux/lua/user/settings.lua
+++ termux/lua/user/settings.lua
@@ -1,8 +1,8 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-vim.g.mapleader = "'"
-vim.g.maplocalleader = "'"
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
 
 vim.opt.termguicolors = true
 
