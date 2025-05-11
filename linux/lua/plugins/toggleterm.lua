fc13c07a6e144bda84207d596bddfc0d46fe900a
champnz <amir0ghahremanian@gmail.com>
2025-05-11 14:19:25 +0330

    feat: make toggleterm float

--- linux/lua/plugins/toggleterm.lua
+++ linux/lua/plugins/toggleterm.lua
@@ -3,7 +3,13 @@ local Plugin = { "akinsho/toggleterm.nvim" }
 Plugin.version = "*"
 
 Plugin.config = function()
-	require("toggleterm").setup()
+	require("toggleterm").setup({
+		direction = "float",
+		float_opts = {
+			border = "curved",
+			winblend = 0,
+		}
+	})
 end
 
 return Plugin
